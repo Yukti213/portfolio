ac340525f62d59bb0e8025d3560d3fb0015216d6
Your Name <you@example.com>
2024-11-23 03:29:55 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+        Hello! My name is Yukti, and I am currently pursuing a Bachelor of Vocation (B.Voc) degree in Web Designing at Kalindi College, University of Delhi.
+         I am in my third year and deeply passionate about creating visually appealing and user-friendly websites.
+          Along with honing my technical skills in web development, I enjoy exploring design trends and innovative ideas
+         to craft engaging digital experiences. When I’m not coding or designing, I love indulging in creative hobbies like meme-making,
+          which allows me to showcase my fun and artistic side.
+
+        </p>
+        <br />
+        <h1 className="text-pink-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [COLLEGE] : Kalindi College, DELHI UNIVERSITY    
+
+         [ SCHOOL ] : Kendriya vidyalaya, Janak puri
+        </span>
+        <br />
+        <br />
+        <h1 className="text-pink-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+        I have a strong foundation in programming and web development, with expertise in several languages and technologies. 
+        My skill set includes CSS, which I use to create visually appealing and responsive web designs. 
+        I’m proficient in Python, a versatile language I utilize for various tasks like scripting, data analysis, and backend development.
+        I also have a working knowledge of Java, which I use for object-oriented programming and basic application development.
+         My experience with JavaScript enables me to build dynamic, interactive web interfaces.problem-solving skills Effective communicator
+          and collaborator.
+        </span>
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default About;
